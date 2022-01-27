c624a6790b81ec87437ae82442b471922d136b4c
Abner Freitas <defreitasabner@gmail.com>
2022-01-27 13:19:21 -0300

    Aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
 const appConfig = require('../config.json');
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+const React = require('react');
+import { useRouter } from 'next/router';
 
-function GlobalStyle() {
-  return (
-      <style global jsx>{`
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      list-style: none;
-    }
-    body {
-      font-family: 'Open Sans', sans-serif;
-    }
-    /* App fit Height */ 
-    html, body, #__next {
-      min-height: 100vh;
-      display: flex;
-      flex: 1;
-    }
-    #__next {
-      flex: 1;
-    }
-    #__next > * {
-      flex: 1;
-    }
-    /* ./App fit Height */ 
-  `}</style>
-  );
-}
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -46,26 +20,13 @@ function Titulo(props) {
     );
 }
 
-/* function HomePage() {
-    return (
-    <div>
-        <GlobalStyle />
-        <Titulo tag="h2">Boas vindas, aventureiro!</Titulo>
-        <h2>Discord - Alura RPG</h2>
-    </div>
-    )
-}
-
-export default HomePage; */
-
 export default function PaginaInicial() {
-   const username = 'defreitasabner';
-  // const [username, setUsername] = React.useState('omariosouto');
-  // const roteamento = useRouter();
+  //const username = 'defreitasabner';
+  const [username, setUsername] = React.useState('defreitasabner'); // declarando uma variável e sua mudança
+  const roteamento = useRouter(); // Permite transição de páginas sem refresh da página toda
 
   return (
     <>
-      <GlobalStyle/>
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -92,18 +53,18 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
-            onSubmit={function (infosDoEvento) {
-              infosDoEvento.preventDefault();
+            onSubmit={function (event) {
+              event.preventDefault();
               console.log('Alguém submeteu o form');
-              //roteamento.push('/chat');
-              window.location.href = '/chat';
+              roteamento.push('/chat');
+              //window.location.href = '/chat'; // dessa forma fica dando refresh
             }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Boas vindas, aventureiro!</Titulo>
+            <Titulo tag="h2">Bem-vindo, aventureiro!</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
